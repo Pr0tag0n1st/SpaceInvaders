a874e5a2a93eab1192403bc8f765fc7e2f2e0536
Tj Martinez <Tj_Martinez01@hotmail.com>
2019-03-20 15:10:57 -0600

    ideas

--- Project/Project/Missle.cpp
+++ Project/Project/Missle.cpp
@@ -29,11 +29,12 @@ void missile::initMissile(int x, int y) {
 }
 
 void missile::move() {
-	timer+=.1;
-	if (timer >3.14*2)
-		timer = -3.14*2;
-	xPos += sin(timer)*5;
-	yPos += cos(timer)*5+1;
+//	timer+=.1;
+	//if (timer >3.14*2)
+		//timer = -3.14*2;
+	//xPos += sin(timer)*5;
+	//yPos += cos(timer)*5+1;
+	yPos += 8;
 	if (yPos > SCREEN_H)
 		alive = false;
 }
